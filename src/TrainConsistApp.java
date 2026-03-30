d30ebc9f84eccb81b2384c301f014c967e97b866
saksham301205 <skmbegad@gmail.com>
2026-03-30 14:53:26 +0530

    UC1: Application entry and welcome message

---
+++ src/TrainConsistApp.java
@@ -0,0 +1,11 @@
+public class TrainConsistApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Train Consist Management System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("====================================");
+
+    }
+}
